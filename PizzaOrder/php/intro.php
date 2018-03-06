3860366c0e4c2c1b99f4d66de271bbba2e32f2ce
Aaron Thawe <aaron.thawe@gmail.com>
2018-03-06 13:59:35 -0500

    php work
    php file now does both MySQL queries, returns JSON of address info for users with email input. TODO: work on JS file to accomodate input and present user with address form to move forward.

--- PizzaOrder/php/intro.php
+++ PizzaOrder/php/intro.php
@@ -1,6 +1,54 @@
 <?php
-header("Content-Type: application/json");
+//header("Content-Type: application/json");
+session_start();
+$_POST['emailInput'] = "wow@gmail.com";
+$db_conn = connect_db();
 
-echo '{ "userEmail": "'.$_POST['emailInput'].'", "testText": "howdy from the php code!"}';
+$qry = <<<END
+select cusID from customer where email = "{$_POST['emailInput']}";
+END;
+$rs = $db_conn->query($qry);
+
+if ($rs->num_rows > 0){
+	$id = array("status" => "OK");
+    while ($row = $rs->fetch_assoc()){
+		array_push($id, $row);
+    }
+    $customerID = $id[0]['cusID'];
+    //echo json_encode($id);
+    //LOOKS LIKE: {"status":"OK","0":{"cusID":"2"}}
+    
+    $qry2 = <<<END2
+    select distinct addr, city, prov, post, appt from orders where cusID = {$customerID};
+END2;
+    $rs2 = $db_conn->query($qry2);
+    if ($rs2->num_rows > 0){
+        $posts = array("status" => "OK");
+        $posts['posts'] = array();
+        while ($row2 = $rs2->fetch_assoc()){
+            array_push($posts['posts'], $row2);
+        }
+        echo json_encode($posts);
+    }
+} else {
+    echo '{ "status": "None" }';
+}
+disconnect_db($db_conn);
+
+//echo '{ "userEmail": "'.$_POST['emailInput'].'", "testText": "howdy from the php code!"}';
 //echo "howdy from the php code!"; 
-//echo '{ "status": "OK","test":"howdy from the php code!" }';
+
+function connect_db(){
+    $db_conn = new mysqli('localhost', 'lamp1user', '!Lamp1!', 'group5');
+    if ($db_conn->connect_errno) {
+        printf ("Could not connect to database server\n Error: "
+            .$db_conn->connect_errno ."\n Report: "
+            .$db_conn->connect_error."\n");
+        die;
+    }
+    return $db_conn;
+}
+
+function disconnect_db($db_conn){
+    $db_conn->close();
+}
--- PizzaOrder/scripts/AaronScript.js
+++ PizzaOrder/scripts/AaronScript.js
@@ -9,6 +9,7 @@ function loadFirstPage(){
     </FORM>`;
 
     $("#introForm").submit(function(event){
+        //add logic here to test for if user entered no email address or invalid, to reload page, else run below
         $.post("php/intro.php", $(this).serialize(), onEntry);
         event.preventDefault();
     })
@@ -19,6 +20,7 @@ var onEntry = function(response){
     $("#error").html("No posts available");
  } else {
     //$("#outputDiv").append(response.status);
+    //currently just shows the email address. change intro.php to run queries on valid email addresses. Then here will be a function call to the next function that will change the outputDiv to the content for the next page.
     $("#test").append(response.userEmail);
  }
 };
