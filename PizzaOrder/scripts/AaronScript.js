c61920d513bb49e11a2769f28f2da88bd06334d0
Aaron Thawe <aaron.thawe@gmail.com>
2018-03-08 00:20:33 -0500

    Further JS work
    completed extraction of address from php into JSON. stored in global variable

--- PizzaOrder/scripts/AaronScript.js
+++ PizzaOrder/scripts/AaronScript.js
@@ -1,9 +1,22 @@
-/*definition of CurrentOrder Object 
-    Purpose:
+/*Definition of CurrentOrder Object 
+    Purpose: To contain all data (in string or JSON format) about the current user and their order and be safely passed around in AJAX calls.
     Properties:
-    Methods:*/
-class CurrentOrder {
-    
+*/
+var currentOrder = {
+    //user ID
+    uID = -1,
+    //user address info
+    address = {status:"default"},
+    newAddress = " ",
+    //an array of pizzas
+    pizzas = []
+};
+function Pizza() {
+    Pizza.size = "";
+    Pizza.dough = "";
+    Pizza.sauce = "";
+    Pizza.cheese = "";
+    Pizza.toppings = "";
 }
 function loadFirstPage(){
     document.getElementById("outputDiv").innerHTML = `        <h3>Welcome!</h3>
@@ -21,13 +34,28 @@ function loadFirstPage(){
 }
 var onEntry = function(response){
  if(response.status == "None"){
-    $("#error").html("No posts available");
+    displayAddressPage();
  } else {
-    //$("#outputDiv").append(response.status);
-    //currently just shows the email address. change intro.php to run queries on valid email addresses. Then here will be a function call to the next function that will change the outputDiv to the content for the next page.
-    $("#test").append(response.addresses[0].addr);
     
+    var addressArray = [];
+    //$("#outputDiv").append(response.status);
+    //$("#test").append(response.addresses[0].addr);
+    for (var x in response.addresses) {
+        //$("#error").append(JSON.stringify(response.addresses[x]));
+        addressArray.push(JSON.stringify(response.addresses[x]));
+    }
+    $("#test").append(addressArray);
+    displayAddressPage(addressArray);
  }
 };
 
-function displayAddressPage()
+function displayAddressPage(addressArray) {
+    //if no previous addresses set, show user just address entry form
+    if(!isset(addressArray)){
+        displayAddForm();
+    } else { //else show user previous used addresses associated with their email, as well as address entry form
+        //output code
+        displayAddForm();
+    }
+    //AJAX here to set up submit button to move forward ALSO to 
+}
